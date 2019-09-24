bd31dac2322c320dad0c2fd84f9372957eb56547
Alex <iliev.alex@outlook.com>
2019-09-24 16:28:22 -0700

    moving all the card components into its own file

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import {Card} from '../card/card.component'
 import './card-list.styles.css';
+
 
 export const CardList = props => (
 	
 	<div className='card-list'>	
- 		{
-        	props.players.map(player => (
-            <h1 key={player.id}> {player.name} </h1>))
-        }
+        	{props.players.map(player => (
+            <Card key={player.id} player={player} />
+        ))}
 	</div>
 )
