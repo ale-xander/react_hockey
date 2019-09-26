6ba916e22cf0a48068fc4d440113c3601b8e565c
Alex <iliev.alex@outlook.com>
2019-09-26 14:56:15 -0700

    destructured Search

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
-import {CardList} from './components/card-list/card-list.component'
+import {CardList} from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-box.component';
 
 class App extends Component{
   constructor(){
@@ -24,10 +25,9 @@ class App extends Component{
         )
     return(
       <div className="App">
-        <input 
-          type='search' 
-          placeholder='search players' 
-          onChange={e => this.setState({searchField: e.target.value})}
+        <SearchBox 
+          placeholder = 'search players'
+          handleChange = {e => this.setState({searchField: e.target.value})}
         />
         <CardList players={filteredPlayers} />
          
