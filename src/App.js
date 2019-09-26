5a4e96aafa9bf2e94331f104017d8eaca322ebcf
Alex <iliev.alex@outlook.com>
2019-09-26 13:57:14 -0700

    adding search functionality

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class App extends Component{
   constructor(){
     super();
     this.state = {
-      players: []
+      players: [],
+      searchField: ''
     }
   }
 
@@ -19,6 +20,11 @@ class App extends Component{
   render(){
     return(
       <div className="App">
+        <input 
+          type='search' 
+          placeholder='search players' 
+          onChange={e => this.setState({searchField: e.target.value})}
+        />
         <CardList players={this.state.players} />
          
       
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import './card.styles.css'
 
-export const Card = (props) =>(
-	<div>
-		<h1> {props.player.name} </h1>
+export const Card = props =>(
+	<div className='card-container'>
+		<img alt='player' src={'http://robohash.org/${props.player.id}?set=set1&size=180x180'} />
+		<h2> {props.player.name} </h2>
+		<p> ID: {props.player.id}</p>
 	</div>
 )
