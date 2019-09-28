d314fe4e8fc97b764e51c90c2be7f95baf1105a0
Alex <iliev.alex@outlook.com>
2019-09-28 16:40:12 -0700

    Updates

---
+++ precache-manifest.296cba1479c0bb7c62375aec32b9dd79.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6d152f561d26633da1ff5ff953b6620",
+    "url": "/react_hockey/index.html"
+  },
+  {
+    "revision": "d978d47a3f4d6cbc5e52",
+    "url": "/react_hockey/static/css/main.14729ef3.chunk.css"
+  },
+  {
+    "revision": "783bb2d55f15e42d1998",
+    "url": "/react_hockey/static/js/2.f3e65479.chunk.js"
+  },
+  {
+    "revision": "d978d47a3f4d6cbc5e52",
+    "url": "/react_hockey/static/js/main.c7d1ca43.chunk.js"
+  },
+  {
+    "revision": "1932f07c3341fb2b326e",
+    "url": "/react_hockey/static/js/runtime-main.ce08fdd5.js"
+  }
+]);
